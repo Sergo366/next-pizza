dd78b8ee10fa7848c2a230bb67ff02da579cf93f
skozenkov <skozenkov@deloitte.com>
2024-11-18 17:55:06 +0100

    Add layout, components, filtration and basic view

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,35 @@
-import {Button} from "@/components/ui/button";
+import {Container} from "@/components/shared/container";
+import {Title} from "@/components/shared/title";
+import {TopBar} from "@/components/shared/topBar";
+import {Filters} from "@/components/shared/filters";
 
 export default function Home() {
   return (
-      <div>Home page
-        <Button>Button</Button>
-      </div>
+      <>
+         <Container className={'mt-10'}>
+            <Title size={'lg'} text={'All pizzas'} className={'font-extrabold'}/>
+        </Container>
+
+          <TopBar/>
+
+         <Container className="pb-14 mt-10">
+             <div className="flex gap-[60px]">
+                 <div className="w-[250px]">
+                     <Filters />
+                 </div>
+                 <div className="flex-1">
+                     {/*<div className="flex flex-col gap-16">*/}
+                     {/*    <ProductsGroupList title="Pizzas" items={[1, 2, 3, 4, 5]} />*/}
+                     {/*    <ProductsGroupList title="Mixed" items={[1, 2, 3, 4, 5]} />*/}
+                     {/*</div>*/}
+
+                     {/*<div className="flex items-center gap-6 mt-12">*/}
+                     {/*    <Pagination pageCount={3} />*/}
+                     {/*    <span className="text-sm text-gray-400">5 из 65</span>*/}
+                     {/*</div>*/}
+                 </div>
+             </div>
+        </Container>
+      </>
   );
 }
