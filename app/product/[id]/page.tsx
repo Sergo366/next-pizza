c5e2caae7d9184f2699d4ae21bf56a85745b201a
skozenkov <skozenkov@deloitte.com>
2024-12-02 20:12:31 +0100

    product image

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import {prisma} from "@/prisma/prisma-client";
+import {notFound} from "next/navigation";
+import {Container} from "@/components/shared/container";
+import {ProductImage} from "@/components/shared/productImage";
+import {Title} from "@/components/shared/title";
 
 type ProductPageProps = {
     params: {
@@ -6,11 +11,25 @@ type ProductPageProps = {
     }
 }
 
-const ProductPage = ({ params: { id } }: ProductPageProps) => {
+const ProductPage = async ({ params: { id } }: ProductPageProps) => {
+    const product = await prisma.product.findFirst({
+        where: {
+            id: Number(id)
+        }
+    })
+
+    if(!product) notFound();
+
     return (
-        <div>
-            Product {id}
-        </div>
+        <Container className={'flex flex-col my-10'}>
+            <div className={'flex flex-1'}>
+                <ProductImage size={40} scr={product.imageUrl} alt={product.name} />
+                <div className={'w-[490px] bg-[#FCFCFC]'}>
+                    <Title text={product.name} size={'md'} className={'font-extrabold mb-1'}/>
+                    <p className={'text-gray-400'}>Lorem ipsum dolor sit amet.</p>
+                </div>
+            </div>
+        </Container>
     );
 };
 
