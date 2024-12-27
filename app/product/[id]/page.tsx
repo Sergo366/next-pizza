3db3e44eadb4065870f051502f67f69a8fbca7eb
skozenkov <skozenkov@deloitte.com>
2024-12-27 16:28:55 +0100

    GroupVariants

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -4,6 +4,7 @@ import {notFound} from "next/navigation";
 import {Container} from "@/components/shared/container";
 import {ProductImage} from "@/components/shared/productImage";
 import {Title} from "@/components/shared/title";
+import {GroupVariants} from "@/components/shared/groupVariants";
 
 type ProductPageProps = {
     params: {
@@ -24,9 +25,17 @@ const ProductPage = async ({ params: { id } }: ProductPageProps) => {
         <Container className={'flex flex-col my-10'}>
             <div className={'flex flex-1'}>
                 <ProductImage size={40} scr={product.imageUrl} alt={product.name} />
-                <div className={'w-[490px] bg-[#FCFCFC]'}>
+                <div className={'w-[490px] bg-[#f7f6f5]'}>
                     <Title text={product.name} size={'md'} className={'font-extrabold mb-1'}/>
                     <p className={'text-gray-400'}>Lorem ipsum dolor sit amet.</p>
+
+                    <GroupVariants
+                        selectedValue={'1'}
+                        items={[
+                        { name: 'Small', value: '1' },
+                        { name: 'Middle', value: '2' },
+                        { name: 'Big', value: '3' },
+                    ]} />
                 </div>
             </div>
         </Container>
